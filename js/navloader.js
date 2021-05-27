c045dbb506d03243144eb11e1623d39f99403035
Billy Ashmall <billy.ashmall@nasa.gov>
2021-05-27 12:09:04 -0500

    add active to nav

--- js/navloader.js
+++ js/navloader.js
@@ -2,3 +2,13 @@
     $("#nav").load("navbar.html");
     $("#footer").load("footer.html");
 });
+
+function setActive(which) {
+    if ($("#" + which).length) {
+        $("#" + which).addClass("active");
+    } else {
+        setTimeout(function () {
+            setActive(which)
+        }, 200);
+    }
+}
